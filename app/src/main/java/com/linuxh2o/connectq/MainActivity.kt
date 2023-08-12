01eb5e5b683452d72f373bac7bc20d04766a8793
TakshakSh <takshak.1996@gmail.com>
2023-08-12 10:29:14 +0530

    updated with firebase

--- app/src/main/java/com/linuxh2o/connectq/MainActivity.kt
+++ app/src/main/java/com/linuxh2o/connectq/MainActivity.kt
@@ -3,11 +3,14 @@ package com.linuxh2o.connectq
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.linuxh2o.connectq.ui.theme.ConnectQTheme
@@ -19,25 +22,25 @@ class MainActivity : ComponentActivity() {
       ConnectQTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          HomeScreen()
         }
       }
     }
   }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-  ConnectQTheme {
-    Greeting("Android")
+fun HomeScreenPreview(){
+  HomeScreen()
+}
+
+@Composable
+fun HomeScreen(){
+  Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+  ) {
+    Text(text = "ConnectQ")
   }
 }
